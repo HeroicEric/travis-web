4800ff21d7de87bf7d4af11c2f6a78e3e7571f09
Emmanuel Patrick <emmanuel@travis-ci.org>
2019-07-04 10:31:56 +0100

    Refactor billing-information to remove side effect in computed property

---
+++ config/plans.js
@@ -0,0 +1,22 @@
+const plans = [
+  {
+    name: 'Bootstrap',
+    enabled: true
+  },
+  {
+    name: 'Startup',
+    enabled: true,
+    isDefault: true
+  },
+  {
+    name: 'Small Business',
+    enabled: true
+  },
+  {
+    name: 'Premium',
+    enabled: true
+  }
+];
+
+
+module.exports = plans;
